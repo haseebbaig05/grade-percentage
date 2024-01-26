7f00430b90a17a6c00786edb72de66b31159c0f3
haseebbaig05 <154249225+haseebbaig05@users.noreply.github.com>
2024-01-26 10:53:10 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+alert('hi')
 
 function calculate()
 {
@@ -8,40 +8,42 @@ function calculate()
     var c=parseInt(document.getElementById('bookthree').value);
     var d=parseInt(document.getElementById('bookfour').value);
     var e=parseInt(document.getElementById('bookfive').value);
-    if (a>100 || b>100 || c>100  || d>100  ||e>100){
+    var f=parseInt(document.getElementById('booksix').value);
+    var g=parseInt(document.getElementById('bookseven').value);
+    if (a>100 || b>100 || c>100  || d>100  ||e>100 ||f>100 ||g>100){
         alert("please enter correct value");
     }
     else
     {
-        var obtain=a+b+c+d+e;
+        var obtain=a+b+c+d+e+f+g;
         document.getElementById("obtain").innerHTML=obtain;
-        var per=obtain/500*100;
+        var per=obtain/550*100;
         document.getElementById("per").innerHTML=per;
-        if(a>40 && b>40 && c>40  && d>40  && e>40){
-            document.getElementById("remarks").innerHTML="<span style='color:#292'> pass</span>";
+        if(a>40 && b>40 && c>40  && d>40  && e>40 && f>40 && g>40){
+            document.getElementById("remarks").innerHTML="<span style='color:#292'>Pass</span>";
         }
     else 
      {
-        document.getElementById("remarks").innerHTML= "<span style='color:red'>fail</span>";
+        document.getElementById("remarks").innerHTML="<span style='color:red'>Fail</span>"; 
 
      }  
      if (per>=80){
         document.getElementById("grade").textContent="A";
      }
      
-     if (per>=70){
+    else if (per>=70){
         document.getElementById("grade").textContent="B";
      }
      
-     if (per>=60){
+    else if (per>=60){
         document.getElementById("grade").textContent="C";
      }
      
-     if (per>=50){
+    else if (per>=50){
         document.getElementById("grade").textContent="D";
      }
      
-     if (per>=40){
+    else if (per>=40){
         document.getElementById("grade").textContent="E";
      }
      else
@@ -49,5 +51,5 @@ function calculate()
 
      }
 
-   } 
+   }
 }
